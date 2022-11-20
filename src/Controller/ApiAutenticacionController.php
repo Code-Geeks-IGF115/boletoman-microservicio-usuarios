9686555cf6c4fb51f4f313d1035cfc576b6a0406
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2022-11-20 08:23:32 +0000

    autenticación completa

--- src/Controller/ApiAutenticacionController.php
+++ src/Controller/ApiAutenticacionController.php
@@ -12,7 +12,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Service\ResponseHelper;
 use App\Repository\UsuarioRepository;
 use App\Entity\Usuario;
-use Symfony\Component\Validator\Constraints\Regex;
 
 class ApiAutenticacionController extends AbstractController
 {
@@ -60,23 +59,10 @@ class ApiAutenticacionController extends AbstractController
     {
         return $this->JWTManager->create($user);
     }
-    #[Route('/api/login', name: 'api_login')]
-    public function login(Request $request): Response
-    {
-        $user=$this->getUser();
-        if ($user==null) {
-            return $this->json("Usuario o contraseña no válidos",Response::HTTP_UNAUTHORIZED);
-        }
-    
-        $token ="Token Generado";
 
-        return $this->json([
-            'message' => 'Usuario Autenticado',
-            'user'  => $user->getUserIdentifier(),
-            'token' => $token]);
-    }
     #[Route('/api/login_check', name: 'api_login_check')]
     public function login_check(){}
+
     #[Route('/api/test', name: 'api_test')]
     public function test(Request $request): JsonResponse
     {
@@ -88,6 +74,7 @@ class ApiAutenticacionController extends AbstractController
         return $this->json([
             'message' => 'TEst',
             'user'  => $user->getUserIdentifier(),
+            'token'=> $this->getTokenUser($user)
         ]);
     }
 }
